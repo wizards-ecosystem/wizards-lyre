4ca96753feb225493b734cd132efb90d28732870
Wizard's Conclave <conclave@local>
2026-08-19 23:00:00 -0600

    [conclave] wip checkpoint job 8

---
+++ web/.dev-vendor/react-dom.js
@@ -0,0 +1,16 @@
+import { _ as __mod } from './chunk-D0qzBqDT.js';
+
+const __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED = __mod["__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED"];
+const createPortal = __mod["createPortal"];
+const createRoot = __mod["createRoot"];
+const findDOMNode = __mod["findDOMNode"];
+const flushSync = __mod["flushSync"];
+const hydrate = __mod["hydrate"];
+const hydrateRoot = __mod["hydrateRoot"];
+const render = __mod["render"];
+const unmountComponentAtNode = __mod["unmountComponentAtNode"];
+const unstable_batchedUpdates = __mod["unstable_batchedUpdates"];
+const unstable_renderSubtreeIntoContainer = __mod["unstable_renderSubtreeIntoContainer"];
+const version = __mod["version"];
+
+export { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED, createPortal, createRoot, __mod as default, findDOMNode, flushSync, hydrate, hydrateRoot, render, unmountComponentAtNode, unstable_batchedUpdates, unstable_renderSubtreeIntoContainer, version };
